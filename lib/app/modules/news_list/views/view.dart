9c0c268d553ce07f3765376101f30a93c623480a
user <user@Admin-2.local>
2024-12-13 12:15:14 +0530

    app chg

--- lib/app/modules/news_list/views/view.dart
+++ lib/app/modules/news_list/views/view.dart
@@ -20,7 +20,7 @@ class NewsListPage extends GetView<NewsListController> {
                 margin: const EdgeInsets.only(top: 20),
                 child: Image.asset(
                   "assets/images/daily.png",
-                  height: 60,
+                  height: 40,
                 )),
             Center(
               child: InkWell(
@@ -29,34 +29,48 @@ class NewsListPage extends GetView<NewsListController> {
                 },
                 child: Container(
                   margin: const EdgeInsets.all(10),
-                  height: Get.height * 0.80,
+                  height: Get.height * 0.70,
                   child: Obx(() => controller.article.isNotEmpty
                       ? ListView.builder(
                           itemCount: controller.article.length,
                           itemBuilder: (ctx, i) {
                             NewsArticle data = controller.article[i];
-                            return ListTile(
-                              title: Text(data.title),
-                              onTap: () async {
-                                final Uri url = Uri.parse(data.link);
-                                await launchUrl(url);
-                              },
-                              trailing: Container(
-                                decoration: BoxDecoration(
-                                  color: Colors.green,
-                                  borderRadius: BorderRadius.circular(20),
-                                  // border: Border.all(color: Colors.black),
-                                ),
-                                padding: const EdgeInsets.all(5),
-                                margin: const EdgeInsets.all(5),
-                                child: const Text(
-                                  "Click Here",
-                                  style: TextStyle(
-                                      color: Colors.white, fontSize: 16),
-                                ),
-                              ),
-                              subtitle: Text(controller.title),
-                            );
+                            return controller.isDownloading
+                                ? Column(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: [
+                                      const CircularProgressIndicator(),
+                                      const SizedBox(height: 20),
+                                      Text(controller.progress),
+                                    ],
+                                  )
+                                : ListTile(
+                                    title: Text(data.title),
+                                    onTap: () async {
+                                      // final Uri url = Uri.parse(data.link);
+                                      // await launchUrl(url);
+                                      controller.downloadFile(
+                                          controller
+                                              .convertToDirectDownloadLink(
+                                                  data.link),
+                                          data.title);
+                                    },
+                                    trailing: Container(
+                                      decoration: BoxDecoration(
+                                        color: Colors.green,
+                                        borderRadius: BorderRadius.circular(20),
+                                        // border: Border.all(color: Colors.black),
+                                      ),
+                                      padding: const EdgeInsets.all(5),
+                                      margin: const EdgeInsets.all(5),
+                                      child: const Text(
+                                        "Click Here",
+                                        style: TextStyle(
+                                            color: Colors.white, fontSize: 16),
+                                      ),
+                                    ),
+                                    subtitle: Text(controller.title),
+                                  );
                           })
                       : const Center(child: CircularProgressIndicator())),
                 ),
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'dart:async';
 
-import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
@@ -16,19 +14,6 @@ Future<void> main() async {
   await GetStorage.init();
 
   await Upgrader.clearSavedSettings();
-
-  if (kIsWeb) {
-    await Firebase.initializeApp(
-      options: const FirebaseOptions(
-          apiKey: "AIzaSyD2PKaXnyxn3gVFcYK_DViXogAEJmE7IyU",
-          authDomain: "dailynews-bbcb0.firebaseapp.com",
-          projectId: "dailynews-bbcb0",
-          storageBucket: "dailynews-bbcb0.appspot.com",
-          messagingSenderId: "334579430217",
-          appId: "1:334579430217:web:565378f63f3283bac1a098",
-          measurementId: "G-5N8LL15TH6"),
-    );
-  }
 
   runApp(
     App(),
